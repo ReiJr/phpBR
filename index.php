ac9c22ba64a64760eadf1c0e712f08eba187d9b7
ReiJr <reinaldo.isj@hotmail.com>
2018-09-13 09:46:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
 if($method == 'POST'){
-	$requestBody = file_get_contents('php://output');
+	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
 	$text = $json->queryResult->parameters->text;
